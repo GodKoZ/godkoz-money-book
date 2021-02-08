29d2f7cd77b76ca8c3cb86232c901b3891b907e6
Ohm Piromrak <ohmpzzz@gmail.com>
2021-02-08 23:06:44 +0700

    refactor webpack optimized

--- server/godkoz-api/webpack.prod.js
+++ server/godkoz-api/webpack.prod.js
@@ -48,7 +48,14 @@ module.exports = {
     rules: [
       {
         test: /\.ts?$/,
-        use: 'ts-loader',
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
         exclude: /node_modules/,
       },
     ],
